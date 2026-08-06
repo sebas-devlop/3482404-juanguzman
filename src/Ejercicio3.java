b867505d459e563fcda0d07408c7e4e8266a7375
XantBuildd <xantbuildd@gmail.com>
2026-08-06 10:05:06 -0500

    Ejercicio3 : mayor - menor - array

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -4,7 +4,7 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("¿Cuántos números va a ingresar?: ");
+        System.out.print("¿Cuántos números va a ingresar?.: ");
         int n = sc.nextInt();
 
         int[] numeros = new int[n];
